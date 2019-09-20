6f732a98108893ad154d744df1daa689bf80c5b2
Miguel Puig <Miguel.PuigGarcia@ig.com>
2019-09-20 17:44:38 +0100

    parcel changes

--- dist/main.cea5deef.js
+++ dist/main.cea5deef.js
@@ -2927,7 +2927,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54916" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56022" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -1,25 +1,109 @@
-console.log('hello world')
-const axios = require('axios');
-const Mustache = require("mustache"); 
+import "babel-polyfill"
+const axios = require('axios')
+const Mustache = require("mustache")
+const apiContent = 'https://chartwell-applications.netlify.com/.netlify/functions/content-get-all'
+const cacheName = 'chartwell-cache'
 
-function loadTranslations() {
-    var template = $('#template').html();
-    Mustache.parse(template);   // optional, speeds up future uses
-    var rendered = Mustache.render(template, {name: "Luke"});
-    $('#target').html(rendered);
-  }
-  loadTranslations()
+class Storage {
+    constructor(name) {
+      this.cacheName = name
+      this.cachePromise = caches.open(name);
+    }
   
-// Make a request for a user with a given ID
-axios.get('https://chartwell-applications.netlify.com/.netlify/functions/content-get-all')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+    async getContent(req) {
+
+      const cache = await this.cachePromise;
+      console.log('cache',cache)
+      return cache.match(req)
+    }
+
+    async cacheAddItem(req, response) {
+        const cache = await this.cachePromise
+        return cache.add(req, response)
+    }
+
+    async deleteContent(req) {
+        const cache = await this.cachePromise
+        return cache.delete(req)
+    }
+
+    async deleteCache() {
+        const deleted = await caches.delete(this.cacheName);
+        return deleted
+    }
+
+    async getCaches() {
+        const cacheNames = await caches.keys();
+        return cacheNames
+    }
+}
+
+class Webcontent {
+    constructor(lang) {
+        this.lang = lang
+        this.content = []
+        this.template = $('#template').html()
+        this.rendered = ''
+        this.target = $('#target')
+    }
+    getKeysContent() {
+        let templateVars = {}
+        for (let i = 0; i < this.content.length; i++){
+            if(this.content[i].data.language === this.lang){
+                templateVars[this.content[i].data.id] = this.content[i].data.value
+            }
+        }
+        return templateVars
+    }
+
+    render(data) {
+        this.content = data.webcontent
+        const keys = this.getKeysContent()  
+        this.rendered = Mustache.render(this.template, keys)
+        this.target.html(this.rendered)
+    }
+    async getAllTranslationsHttp () {
+        let data = await fetch(apiContent)
+        return data
+    }
+}
+
+
+const content = new Webcontent('en_GB');
+const storage = new Storage(cacheName);
+Mustache.parse(content.template)
+
+if ('caches' in window) {    
+    storage.getContent(apiContent).then(function(response) {
+
+        if(response){
+            console.log(response)
+            const date = new Date(response.headers.get('date'))
+            console.log('date',response.headers.get('Date'))
+            //Data is in template
+            response.json().then(function(result) {
+                //Render in template
+                content.render(result)
+        });
+        }else{
+            content.getAllTranslationsHttp().then(function(response) {
+                //Render in template
+                response.json().then(function(result) {
+                    content.render(result)
+                });
+                //Add item to cache
+                storage.cacheAddItem(apiContent, response)
+            })
+        }
+    });
+}else{
+
+}
+
+
+  
+
+
+
+  
+
